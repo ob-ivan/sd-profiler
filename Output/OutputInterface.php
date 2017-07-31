f3f3d89b84a958b7a4d0caa7b1463e00c5e34b30
Ivan Vegner <vegner@slrealty.ru>
2017-07-31 12:08:54 +0300

    Fix classname import

--- Output/OutputInterface.php
+++ Output/OutputInterface.php
@@ -2,7 +2,9 @@
 
 namespace SD\Profiler\Output;
 
+use SD\Profiler\Frame;
+
 interface OutputInterface {
     public function init($config);
-    public function process(SD\Profiler\Frame $frame);
+    public function process(Frame $frame);
 }
